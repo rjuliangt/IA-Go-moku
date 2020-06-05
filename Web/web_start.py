329f2773e36447b897f387fe8f01377b18651297
laptop_julian <juliorob7@gmail.com>
2020-06-04 21:46:15 -0600

    maquetado de barra de navegacion realizada

--- Web/web_start.py
+++ Web/web_start.py
@@ -1,10 +1,1 @@
-from flask import Flask, jsonify
 
-app =  Flask(__name__)
-
-@app.route('/', methods = ['GET'])
-def inicio():
-    return jsonify({"Si dio xd":"xdede"})
-
-if __name__ == '__main__':
-    app.run()

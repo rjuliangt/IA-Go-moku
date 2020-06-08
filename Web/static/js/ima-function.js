4f9e5eb87b9b37f47fd70ff120104ae52106a5e2
laptop_julian <juliorob7@gmail.com>
2020-06-07 21:44:26 -0600

    funscion de java script

--- Web/static/js/ima-function.js
+++ Web/static/js/ima-function.js
@@ -1,17 +1,18 @@
-function inicio()
-{
-    var nuevaImagen = new Image();
-    alert("Se procede a la carga en memoria de la imagen");
-    nuevaImagen = cargarImagen("ejemplo.png");
-}
-function cargarImagen(url)
-{
-    var imagen = new Image();
-    imagen.onload = imagenCargada;
-    imagen.src = url;
-    return imagen;
-}
-function imagenCargada()
-{
-    alert("La imagen se ha cargado correctamente");
+var id = document.getElementById("ID_del_div").innerHTML='<img src="./direccion_imagen.jpg" />';
+document.getElementsByClassName()
+
+function addIma() {
+
+    var Tbl = document.getElementById("board");
+    var tblBody = Tbl.getElementsByTagName("tbody");
+
+    var fila = document.getElementsByClassName("tr");
+    var columna = document.getElementsByClassName("td");
+
+
+    celda.appendChild(textoCelda);
+    hilera.appendChild(celda);
+    tblBody.appendChild(hilera);
+    Tbl.appendChild(tblBody);
+
 }

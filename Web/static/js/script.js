3d0c724a2491ebb36a8627c68227f034ea8b4ee5
laptop_julian <juliorob7@gmail.com>
2020-06-09 11:47:03 -0600

    testeando funcion para agregar imagenes en js

---
+++ Web/static/js/script.js
@@ -0,0 +1,16 @@
+// var id = document.getElementById("ID_del_div").innerHTML='<img src="./direccion_imagen.jpg" />';
+// document.getElementsByClassName()
+
+function addIma() {
+    // var Tbl = document.getElementById("board");
+
+    // var fila = document.getElementsByClassName("tr");
+
+    // var img = document.createElement('img');
+    // img.className = 'player';
+    // img.src = '../ima/delete.ico';
+    // columna.appendChild(img);
+    // fila.appendChild(columna);
+    // tblBody.appendChild(fila);
+    // Tbl.appendChild(tblBody);
+}

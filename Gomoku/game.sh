56e0590ae9de890cb7ec9424fe3765e03266133b
Carlos Vásquez <carls10vasquez@gmail.com>
2020-06-10 04:43:42 -0600

    data game

--- Gomoku/game.sh
+++ Gomoku/game.sh
@@ -4,45 +4,6 @@ then
   exit 0
 fi
 
-echo ""
-echo "Welcome to Gomoku_IA"
-
-while true
-do
-  echo ""
-  echo "Next step, please enter the corresponding number to perform the operation."
-  echo "---------------------------"
-  echo "0: Exit"
-  echo "1: Configure the game"
-  echo "2: Run"
-  echo "3: Run visual game by Web server"
-  echo "4: Neural network training"
-  echo "---------------------------"
-
-  read -p ": " input
-  case $input in
-  0)
-    break
-    ;;
-  1)
-    python3 configure.py
-    continue
-    ;;
-  2)
-    python3 start.py
-    continue
-    ;;
-  3)
-    python3 start_from_web.py
-    continue
-    ;;
-  4)
-    python3 train.py
-    continue
-    ;;
-  *)
-    echo "The input is incorrect, please try again."
-    continue
-    ;;
-  esac
-done
+python3 start.py
+python3 start_from_web.py
+python3 train.py
--- Gomoku/start_from_web.py
+++ Gomoku/start_from_web.py
@@ -1,10 +1,7 @@
 from Web import web_start, app
 
-print("Web http://127.0.0.1:5000. ")
-print("The web server has been started at http://127.0.0.1:5000. Press <Ctrl-C> to close")
-
 try:
-    web_start.socket_io.run(app, host='0.0.0.0')
+    web_start.socket_io.run(app, host='0.0.0.0:5000')
     # app.run(host='0.0.0.0')
 except KeyboardInterrupt:
     # web_start.disconnect()

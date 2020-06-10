f065ea560f34baa91e5951720dea92013bf1f1c4
Carlos Vásquez <carls10vasquez@gmail.com>
2020-06-10 05:12:01 -0600

    5:11

--- Gomoku/start_from_web.py
+++ Gomoku/start_from_web.py
@@ -1,8 +1,8 @@
 from Web import web_start, app
 
 try:
-    web_start.socket_io.run(app, host='0.0.0.0')
-    # app.run(host='0.0.0.0')
+    # web_start.socket_io.run(app, host='0.0.0.0')
+    app.run(host='0.0.0.0')
 except KeyboardInterrupt:
-    # web_start.disconnect()
+    #web_start.disconnect()
     web_start.socket_io.stop()

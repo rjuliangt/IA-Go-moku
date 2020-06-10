0e32829e16deb3c02823fedcd1dc38d3963bba24
Carlos Vásquez <carls10vasquez@gmail.com>
2020-06-10 04:49:02 -0600

    data in game

--- Gomoku/game.sh
+++ Gomoku/game.sh
@@ -3,7 +3,4 @@ then
   python3 test.py
   exit 0
 fi
-
-python3 start.py
 python3 start_from_web.py
-python3 train.py
--- Gomoku/start_from_web.py
+++ Gomoku/start_from_web.py
@@ -1,7 +1,7 @@
 from Web import web_start, app
 
 try:
-    web_start.socket_io.run(app, host='0.0.0.0:5000')
+    web_start.socket_io.run(app, host='0.0.0.0')
     # app.run(host='0.0.0.0')
 except KeyboardInterrupt:
     # web_start.disconnect()

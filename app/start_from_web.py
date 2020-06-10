3c818af7cfaf5277f097d8f2eb85d191eedd496b
Carlos Vásquez <carls10vasquez@gmail.com>
2020-06-10 06:18:42 -0600

    repair

--- app/start_from_web.py
+++
@@ -1,8 +0,0 @@
-from Web import web_start, app
-
-try:
-    # web_start.socket_io.run(app, host='0.0.0.0')
-    app.run()
-except KeyboardInterrupt:
-    web_start.disconnect()
-    #web_start.socket_io.stop()
